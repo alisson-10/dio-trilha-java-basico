afd30abaa9fc903f78c75de465e7abd8ed93466b
alisson-10 <alissonsoaressilva@live.com>
2023-06-28 16:06:32 -0300

    Treinando tipos e variáveis

--- java-variaveis/src/app/TiposVariaveis.java
+++ java-variaveis/src/app/TiposVariaveis.java
@@ -2,4 +2,32 @@ package app;
 
 public class TiposVariaveis {
 
+    boolean falso = false;
+    boolean verdadeiro = true;
+
+public static void main(String[] args) {
+   
+     byte numeroCasas = 10;
+     System.out.println(numeroCasas);
+     
+     short anoFabricacao = 2023;
+     System.out.println(anoFabricacao);
+     
+     String primeiroNome = "Alisson";
+     System.out.println(primeiroNome);
+     
+     final int MELHORANO = 2010;
+     System.out.println(MELHORANO);
+
+     double numeroDecimal = 2.333;
+     System.out.println(numeroDecimal);
+
+     
+    
+
+
+    
+
+
+}
 }
